44a996003947ba893e30fd2db7e58806ebcdf033
unknown <imperia.zt@gmail.com>
2024-05-17 13:35:53 +0300

    gh pages

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -57,7 +57,7 @@ export class Table extends ExcelComponent {
     }
 
     toHTML(){
-        return createTable(20, this.store.getState())}
+        return createTable(50, this.store.getState())}
     
 
     async resizeTable(event){
